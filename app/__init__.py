25e69a05a9c4bf21d436a0381e46e7a13fb231c3
Arseniy Sokolovskiy <sokolovskiy.sn@gmail.com>
2025-06-13 17:59:50 +0300

    Gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import os
+import cloudinary
+
+from flask import Flask, redirect
 from .extensions import db, migrate, cors, jwt, ma
 from .config import config_by_name
 from .models import *
@@ -16,5 +19,16 @@ def create_app():
                   r"/api/*": {"origins": app.config['CORS_ORIGINS']}})
     jwt.init_app(app)
     ma.init_app(app)
+
+    cloudinary.config(
+        cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
+        api_key=os.getenv('CLOUDINARY_API_KEY'),
+        api_secret=os.getenv('CLOUDINARY_API_SECRET')
+    )
+
+    @app.route('/static/<path:filename>')
+    def serve_static(filename):
+        cloudinary_url = cloudinary.utils.cloudinary_url(filename)[0]
+        return redirect(cloudinary_url)
 
     return app
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+# main.py
+import os
 from app import create_app
 
-
-app = create_app()  # 'prod' для продакшена
+app = create_app()  # Создаём объект app
 
 if __name__ == '__main__':
-    print(app)
     from app.apscheduler_tasks import register_scheduler
     with app.app_context():
         register_scheduler(app)
-        app.run()
+        port = int(os.getenv("PORT", 8000))
+        app.run(host="0.0.0.0", port=port)
