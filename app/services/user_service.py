1994ad4214b34d74be1df3fc13f385ea42308db5
Arseniy Sokolovskiy <sokolovskiy.sn@gmail.com>
2025-06-22 13:39:54 +0300

    Trying fix file storing

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -155,7 +155,7 @@ def save_image(file_storage: FileStorage, image_type: str, entity_id=None):
         with open(temp_file_path, 'rb') as f:
             # Передаем только имя файла без пути, путь сохраняется в метаданных
             uploaded_file = uploadcare.upload(
-                f, store=True, metadata={'path': storage_path})
+                f, store=True, metadata={'filename': storage_path})
 
         # Удаляем временный файл
         os.unlink(temp_file_path)
