9beb3a2801eaf41546f409e4053b38fac1ca1867
Arseniy Sokolovskiy <sokolovskiy.sn@gmail.com>
2025-06-28 01:54:53 +0300

    Temp admin adding

--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -1,9 +1,18 @@
-# from flask import Blueprint
-# from app.services.
+from flask import Blueprint, request
+
+from app.models.user_models import User
+from app.extensions import db
 
 
-# admin_bp = Blueprint('admin', __name__, url_prefix='/api/admin')
-
-# @admin_bp.route()
+admin_bp = Blueprint('admin', __name__, url_prefix='/api/admin')
 
 
+@admin_bp.route('make-admin', methods=['POST'])
+def make_admin():
+    data = request.json
+    user_email = data['email']
+    user = User.query.filter_by(email=user_email).first()
+    user.is_admin = True
+
+    db.session.add(user)
+    db.session.commit()
