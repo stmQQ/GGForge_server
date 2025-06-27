a51381ab4e2977de8b81245e47542937b5b83f0f
Arseniy Sokolovskiy <sokolovskiy.sn@gmail.com>
2025-06-27 19:51:27 +0300

    config modified

--- app/config/base.py
+++ app/config/base.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class BaseConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'super-secret-key')
-    CORS_ORIGINS = os.getenv('CORS_ORIGINS', '*')
+    CORS_ORIGINS = [os.getenv('CORS_ORIGINS', '*'), 'http://localhost:5173']
